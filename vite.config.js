f9415269eebae51efa723e949abaf1230c0de78a
karim-codes <abdikariim.a.y@gmail.com>
2024-07-25 13:25:24 +0100

    Fix Rollup issue

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['/src/main.jsx'] // Prevent Rollup from including this module
+    },
+    // Optional: adjust chunk size warning limit if needed
+    chunkSizeWarningLimit: 1000,
+  },
+});
