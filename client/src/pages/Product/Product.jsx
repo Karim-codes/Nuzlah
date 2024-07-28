a7345b448b9d417d806962a02648b59ae0c2d248
karim-codes <abdikariim.a.y@gmail.com>
2024-07-28 13:16:23 +0100

    Build project and add dist directory

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -38,7 +38,7 @@ export const Product = () => {
       <div className="right">
         <h1>{data?.attributes?.title}</h1>
         <span className='price'>£{data?.attributes?.price}</span>
-        <p>{data?.attributes?.description} 
+        <p>{data?.attributes?.desc} 
         </p>
         <div className="quantity">
           <button onClick={()=>setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
@@ -48,7 +48,7 @@ export const Product = () => {
         <button className="add" onClick={()=>dispatch(addToCart({
           id:data.id,
           title:data.attributes.title,
-          description:data.attributes.description,
+          desc:data.attributes.desc,
           price:data.attributes.price,
           img:data.attributes.img.data.attributes.url,
           quantity,
